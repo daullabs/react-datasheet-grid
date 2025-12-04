bf27ec5afb226b378543bf024dd8f4d871477ae7
Jihoon Kang <kang@ghoon.net>
2025-12-04 12:19:32 +0900

    Add dist for hotfix

---
+++ dist/columns/koreanTextColumn.d.ts
@@ -0,0 +1,24 @@
+import { Column } from '../types';
+type KoreanTextColumnOptions<T> = {
+    placeholder?: string;
+    alignRight?: boolean;
+    continuousUpdates?: boolean;
+    deletedValue?: T;
+    parseUserInput?: (value: string) => T;
+    formatBlurredInput?: (value: T) => string;
+    formatInputOnFocus?: (value: T) => string;
+    formatForCopy?: (value: T) => string;
+    parsePastedValue?: (value: string) => T;
+};
+type KoreanTextColumnData<T> = {
+    placeholder?: string;
+    alignRight: boolean;
+    continuousUpdates: boolean;
+    parseUserInput: (value: string) => T;
+    formatBlurredInput: (value: T) => string;
+    formatInputOnFocus: (value: T) => string;
+};
+export declare const koreanTextColumn: Partial<Column<string | null, KoreanTextColumnData<string | null>, string>>;
+export declare function createKoreanTextColumn<T = string | null>({ placeholder, alignRight, continuousUpdates, deletedValue, parseUserInput, formatBlurredInput, formatInputOnFocus, formatForCopy, parsePastedValue, }?: KoreanTextColumnOptions<T>): Partial<Column<T, KoreanTextColumnData<T>, string>>;
+export {};
+//# sourceMappingURL=koreanTextColumn.d.ts.map
